95ed760fa82f98580d19b9d30a98c7738735f601
Jim <294862048a@gmail.com>
2024-11-27 00:26:08 +0100

    Initial commit

---
+++ src/main/java/org/example/asianmarket/AsianMarketApplication.java
@@ -0,0 +1,13 @@
+package org.example.asianmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsianMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsianMarketApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/asianmarket/model/entity/User.java
@@ -0,0 +1,4 @@
+package org.example.asianmarket.model;
+
+public class User {
+}
